c0da1ff21e0be8fbba2b35899d7c6c5b50dcf072
Gabryel-lima <gabbryellimasi@gmail.com>
2023-05-28 20:32:29 -0300

    Atualizando

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
+import sys
 from cx_Freeze import setup, Executable
 
-executables = [Executable("Block-Bricks.py")]
+# Informe o caminho para o seu arquivo principal do jogo
+main_script = 'Block-Bricks.py'
 
+# Informe o nome da pasta que contém os sons
+sounds_folder = 'sounds'
+
+# Configurações do executável
+executables = [Executable(main_script)]
+
+# Opções de configuração
+options = {
+    'build_exe': {
+        'include_files': [sounds_folder],
+        'packages': [],
+        'excludes': []
+    }
+}
+
+# Criação do executável
 setup(
-    name="Block-Bricks",
-    version="1.0",
-    description="Jogo",
+    name='Block-Bricks',
+    version='1.0',
+    description='Muito top!',
+    options=options,
     executables=executables
 )
-
