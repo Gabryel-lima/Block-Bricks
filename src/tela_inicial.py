3f768fc85518d7c7dcfcea0447bbf40ae959b1cc
Gabryel-lima <gabbryellimasi@gmail.com>
2023-06-16 18:05:32 -0300

    Atualizando

--- src/tela_inicial.py
+++ src/tela_inicial.py
@@ -5,8 +5,6 @@ from jogo_base import JogoBase
 class TelaInicial(JogoBase):
     def __init__(self):
         super().__init__()
-        self.cor_botao_modo1 = (255,255,255)
-        self.cor_botao_modo2 = (255,255,255)
-        self.cor_botao_voltar = (255,255,255)
+        pass
 
 
