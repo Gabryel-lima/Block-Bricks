48b6cceed4ab55c080a48492f1eba7e977447baa
Gabryel-lima <gabbryellimasi@gmail.com>
2023-10-15 21:33:25 -0300

    Tentando implementar bot

--- src/bot_ia.py
+++ src/bot_ia.py
@@ -1,21 +1,51 @@
 
+import pandas as pd
+from imblearn.over_sampling import SMOTE
 from sklearn.tree import DecisionTreeRegressor
-from coleta_dados import ColetaDados
+
+class ColetaDados:
+    def __init__(self, json_path):
+        self.json_path = json_path
+
+    def coletar_dados(self):
+        # Carregue o arquivo JSON em um DataFrame
+        df = pd.read_json(self.json_path)
+
+        dado = {
+            'pos_bola': [{'x': 1, 'y': 2, 'acao': 'parado'},
+                        {'x': 3, 'y': 4, 'acao': 'direita'},
+                        {'x': 5, 'y': 6, 'acao': 'esquerda'}]
+        }
+
+        df = pd.DataFrame(dado)
+
+        # Aplique get_dummies à coluna 'pos_bola'
+        dummie_dados = pd.get_dummies(df.drop(['pos_bola'], axis=1))
+        return dummie_dados
 
 class Bot:
     def __init__(self):
-        self.dtr = DecisionTreeRegressor()
-        
-    def treinar_bot(self):
-        # Coletar dados de treinamento usando uma função adequada
-        X_treino, y_treino = ColetaDados()
-        
-        # Treinar o modelo
-        self.dtr.fit(X_treino, y_treino)
+        self.dtr = DecisionTreeRegressor(criterion='squared_error', random_state=50)
+        self.coleta_dados = ColetaDados('src/coletadds.json')
 
-    def tomar_acao(self, estado_jogo):
-        # Usar o modelo treinado para prever a ação com base no estado do jogo
-        acao_numerica = self.dtr.predict(estado_jogo)
-        # Converter ação numérica em ação real
-        acao_real = [acao for acao, valor in self.dict_movimento.items() if valor == acao_numerica]
-        return acao_real
+    def treinar_bot(self):
+        # Coleta os dados
+        dados = self.coleta_dados.coletar_dados()
+
+        # Defina as colunas corretamente
+        X = dados.drop(['pos_bola'], axis=1)
+        y = dados['pos_bola']
+
+        smt = SMOTE(random_state=150)
+        X, y = smt.fit_resample(X, y)
+        
+        # Treine seu modelo DecisionTreeRegressor com X_resampled e y_resampled
+        self.dtr.fit(X, y)
+
+        dados_final = pd.concat([X, y], axis=1)
+
+        print(dados_final.head(2))
+
+if __name__ == "__main__":
+    bot = Bot()
+    bot.treinar_bot()
--- src/coleta_dados.py
+++ src/coleta_dados.py
@@ -1,4 +1,3 @@
-
 import json
 import pygame
 from pygame.locals import K_a, K_d
@@ -7,7 +6,7 @@ from player import Player
 class ColetaDados:
     def __init__(self, tela, borda, largura, altura):
         self.player = Player(tela, borda, largura, altura)
-        self.coletados = []
+        self.coletados = {'dados': []}  # Inicialize como um dicionário com a chave 'dados'
         self.coleta_intervalo = 1000 
         self.coleta_tempo_acumulado = 0
 
@@ -22,10 +21,8 @@ class ColetaDados:
     def coletar_dados(self, pos_x, pos_y):
         acao = self.verifica_direcao()
         pos_bola = (pos_x, pos_y)
-        self.coletados.append({'acao': acao, 'pos_bola': pos_bola})
+        self.coletados['dados'].append({'acao': acao, 'pos_bola': pos_bola})  # Adicione ao dicionário 'dados'
 
     def salva_dados(self):
-        data = {'dados': self.coletados}
         with open('src/coletadds.json', 'w') as file:
-            json.dump(data, file)
-                
+            json.dump(self.coletados, file)  # Salve o dicionário completo
