f6fc30906c3a2226cdc7028d51ed7170c864a5eb
Gabryel-lima <gabbryellimasi@gmail.com>
2023-10-17 13:14:32 -0300

    .

--- src/bot_ia.py
+++ src/bot_ia.py
@@ -10,57 +10,69 @@ class ColetaDados:
         self.csv_path = csv_path
 
     def coletar_dados(self):
-        # Carregue o arquivo CSV em um DataFrame
         df = pd.read_csv(self.csv_path)
-
         return df
 
-class Bot:
-    def __init__(self):
+class TreinarModelo:
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
         self.dtc = DecisionTreeClassifier(random_state=50)
-        self.coleta_dados = ColetaDados('src/coletadds.csv')
+        self.df = None
+        self.X = None
+        self.X_resampled = None
+        self.X_treino = None
+        self.X_teste = None
+        self.y = None
+        self.y_resampled = None
+        self.y_treino = None
+        self.y_teste = None
+        self.cmx = None
+        self.acuracia = None
+        self.precisao = None
+        self.recall = None
+
+    def ler_dados(self):
+        coleta_dados = ColetaDados(self.csv_path)
+        self.df = coleta_dados.coletar_dados()
+        return self.df
 
     def treinar_bot(self):
-        # Coleta os dados
-        dados = self.coleta_dados.coletar_dados()
+        self.dados_df = self.ler_dados()
 
-        # Defina as colunas corretamente
-        X = dados.drop(['acao'], axis=1)
-        y = dados['acao']
+        self.X = self.dados_df.drop(['acao'], axis=1)
+        self.y = self.dados_df['acao']
 
-        smt = SMOTE(random_state=166)
-        X_resampled, y_resampled = smt.fit_resample(X, y)
+        self.smt = SMOTE(random_state=166)
+        self.X_resampled, self.y_resampled = self.smt.fit_resample(self.X, self.y)
 
-        # Dividir os dados em conjuntos de treinamento e teste
-        X_treino, X_teste, y_treino, y_teste = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=43)
+        self.X_treino, self.X_teste, self.y_treino, self.y_teste = train_test_split(self.X_resampled, self.y_resampled, test_size=0.2, random_state=43)
 
-        self.dtc.fit(X_treino, y_treino)
+        self.dtc.fit(self.X_treino, self.y_treino)
 
-        dados_final = pd.concat([X_treino, y_treino], axis=1)
+        dados_final = pd.concat([self.X_treino, self.y_treino], axis=1)
 
         with pd.option_context('display.max_rows', 1312):
             print(dados_final.head(1312))
 
-        predito_Arv = self.dtc.predict(X_teste)
+        predito_Arv = self.dtc.predict(self.X_teste)
 
         # Avaliação do modelo
-        cm = confusion_matrix(y_teste, predito_Arv)
-        print("\nMatriz de Confusão:\n", cm)
+        self.cmx = confusion_matrix(self.y_teste, predito_Arv)
+        print("\nMatriz de Confusão:\n", self.cmx)
 
-        acuracia = accuracy_score(y_teste, predito_Arv)
-        print(f"\nAcuracia do modelo: {100 * acuracia:.2f}%\n")
+        self.acuracia = accuracy_score(self.y_teste, predito_Arv)
+        print(f"\nAcuracia do modelo: {100 * self.acuracia:.2f}%\n")
 
-        precisao = precision_score(y_teste, predito_Arv, average=None)
-        for classe, p in enumerate(precisao):
-            print(f"Precisao da classe {precisao} : {100 * p:.2f}%")
+        self.precisao = precision_score(self.y_teste, predito_Arv, average=None)
+        for classe, p in enumerate(self.precisao):
+            print(f"Precisao da classe {self.precisao} : {100 * p:.2f}%")
 
         print()
 
-        recall = recall_score(y_teste, predito_Arv, average=None)
-        for classe, i in enumerate(recall):
+        self.recall = recall_score(self.y_teste, predito_Arv, average=None)
+        for classe, i in enumerate(self.recall):
             print(f"Recall da classe {classe} : {100 * i:.2f}%")
-        
 
 if __name__ == "__main__":
-    bot = Bot()
-    bot.treinar_bot()
+    treino = TreinarModelo('src/coletadds.csv')
+    treino.treinar_bot()
