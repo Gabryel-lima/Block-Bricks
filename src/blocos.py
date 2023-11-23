38ddc3499ae5730a36e165d4343182328de14bf7
Gabryel-lima <gabbryellimasi@gmail.com>
2023-11-22 22:01:36 -0300

    Atualização de animação

--- src/blocos.py
+++ src/blocos.py
@@ -9,6 +9,7 @@ class Blocos:
     def __init__(self, jogo):
         self.jogo = jogo
         self.cor_blocos = (150,75,0)
+        self.cor_animacao = (250,250,250)
         self.nivel_atual = 0
         self.nsort = random.randint(0,3)
         self.countr = random.randint(2,12)
@@ -40,30 +41,13 @@ class Blocos:
             pygame.draw.rect(self.jogo.tela, self.cor_blocos, bloco, width=0, border_radius=3)
 
     def animacao_blocos(self, index):
-        cor = (250,250,250)
-        if 0 <= index <= len(self.blocos):
-            bloco = self.blocos[index]
-            pygame.draw.rect(self.jogo.tela, cor, bloco, width=0, border_radius=3)
-
-    def remove_indice(self):
-        max_selecionados = min(self.countr, len(self.blocos))
-        if max_selecionados >= len(self.blocos):
-            max_selecionados = len(self.blocos) - 1
-        
-        self.indice_aleatorio = random.sample(range(len(self.blocos)), max_selecionados)
-        print(self.indice_aleatorio)
-        blocos_selecionados = [self.blocos[indice] for indice in self.indice_aleatorio]
-        print(blocos_selecionados)
-
-        self.blocos = [bloco for indice, bloco in enumerate(self.blocos) if indice not in self.indice_aleatorio]
-
-        '''max_selecionados = min(self.countr, len(self.blocos)): Aqui, estamos calculando o número máximo de índices que podem ser selecionados. O valor é definido como o mínimo entre self.countr (que é o valor que determina quantos índices devem ser selecionados) e len(self.blocos) (que é o número total de blocos disponíveis).
-        if max_selecionados >= len(self.blocos):: Nesta linha, estamos verificando se o número máximo de índices selecionados é maior ou igual ao número total de blocos disponíveis. Se isso for verdade, significa que poderíamos acabar selecionando todos os blocos, o que queremos evitar.
-        max_selecionados = len(self.blocos) - 1: Se o número máximo de índices selecionados for maior ou igual ao número de blocos disponíveis, ajustamos o valor de max_selecionados para ser um valor menor, subtraindo 1 do número total de blocos.
-        self.indice_aleatorio = random.sample(range(len(self.blocos)), max_selecionados): Aqui estamos usando a função random.sample para selecionar aleatoriamente max_selecionados índices da lista de blocos (self.blocos). Esses índices selecionados serão armazenados na lista self.indice_aleatorio.
-        blocos_selecionados = [self.blocos[indice] for indice in self.indice_aleatorio]: Nesta linha, estamos criando uma nova lista chamada blocos_selecionados, que contém os blocos correspondentes aos índices selecionados em self.indice_aleatorio.
-        self.blocos = [bloco for indice, bloco in enumerate(self.blocos) if indice not in self.indice_aleatorio]: Aqui estamos atualizando a lista de blocos (self.blocos) para remover os blocos correspondentes aos índices que foram selecionados aleatoriamente e armazenados em self.indice_aleatorio. Estamos usando uma compreensão de lista para criar uma nova lista de blocos que não inclui os blocos que foram selecionados.
-        Essencialmente, esse método seleciona aleatoriamente um número de índices da lista de blocos, armazena esses índices e os blocos correspondentes em listas separadas, e então remove os blocos selecionados da lista original de blocos. Isso permite que você mantenha o controle dos blocos que foram selecionados e removidos para realizar as ações desejadas em seu jogo.'''
+        if 1 <= index <= len(self.blocos):
+            bloco_antigo = self.blocos[index - 1]
+            bloco_novo = bloco_antigo.copy()
+            bloco_novo.move_ip(bloco_antigo.centerx, bloco_antigo.centery)
+            pygame.draw.rect(self.jogo.tela, self.cor_animacao, bloco_antigo, width=0, border_radius=3)
+            pygame.draw.rect(self.jogo.tela, (150,150,100), bloco_novo, width=5, border_radius=3)
+            #pygame.display.update()
 
     def configurar_nivel(self):
         if self.nivel_atual < len(self.niveis):
@@ -75,7 +59,6 @@ class Blocos:
             self.blocos = []
             self.nivel_atual += 1
             self.criar_blocos()
-            #self.remove_indice()
 
     def habilidades(self):
         pass
@@ -84,4 +67,4 @@ class Blocos:
         self.blocos.clear()
         self.nivel = 0
         self.criar_blocos()
-        #self.remove_indice()
+
